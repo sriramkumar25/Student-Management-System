bcd3495a9d70ac7b4451b9c83a8f7f6c8b75f6da
sriramkumar <rmsriram0205@gmail.com>
2023-07-23 20:31:53 +0530

    stu_css

--- client/src/components/StudentLandingPage/Student.jsx
+++ client/src/components/StudentLandingPage/Student.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import './Student.css'
 
 function Student() {
   const { currentUser } = useAuth();
@@ -34,7 +35,62 @@ function Student() {
     //style it like a table of contents
     //it should be able to map with all the contents of all the tests like, class test1, unit test 1 ,etc.
     //return it properly with the use of 'map()' function
-    <>I am a student thaayoli</>
+    <div>
+      <div class="cont">
+        <image class="photo" />
+        <div class="detail">
+          <div class="name">
+            Name:
+          </div>
+          <br />
+          <div class="rollno">
+            Rollno:
+          </div>
+        </div>
+      </div>
+      <br />
+      <div class="cont2">
+        <h2>Test-1 : </h2>
+        <table align="center">
+          <tr>
+            <th>S.No</th>
+            <th>Subject</th>
+            <th>Marks</th>
+            <th>Grade</th>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+    </div>
   );
 }
 
